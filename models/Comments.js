3856cb70e1abfdc3d799dd01467f74bf9cbbeb75
dolan187 <akunivalainen@gmail.com>
2016-07-10 12:00:50 +0300

    Fixed few typos. Created simple rest routes. Moved require mongoose and register models before requiring routes.

--- models/Comments.js
+++ models/Comments.js
@@ -1,4 +1,4 @@
-var mongoose = require('mogoose');
+var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
   body: String,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
+// express
 var express = require('express');
 var router = express.Router();
+// mongoose & models
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET all posts
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+// POST create new Post
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
 });
 
 module.exports = router;
